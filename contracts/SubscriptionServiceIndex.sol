63e6445de0c110891d91e2fd39472638bbd96979
Renat Skitsan <renatskitsan@gmail.com>
2021-11-11 09:49:53 -0800

    WIP

---
+++ contracts/SubscriptionServiceIndex.sol
@@ -0,0 +1,39 @@
+//pragma ton-solidity ^ 0.47.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+
+interface ISubscriptionServiceContract {
+    function selfdelete () external;
+}
+
+contract SubscriptionServiceIndex {
+    ServiceParams public svcparams;
+    struct ServiceParams {
+        address to;
+        uint128 value;
+        uint32 period;
+        string name;
+        string description;
+    }
+    TvmCell public static params;
+    address serviceAddress;
+
+    modifier onlyOwner {
+		require(msg.pubkey() == tvm.pubkey(), 100);
+		tvm.accept();
+		_;
+    }
+
+    constructor(bytes signature,TvmCell svcCode) public {
+        require(msg.sender != address(0), 101);
+        require(tvm.checkSign(tvm.hash(svcCode), signature.toSlice(), tvm.pubkey()), 102);
+        (svcparams.to, svcparams.value, svcparams.period, svcparams.name, svcparams.description) = params.toSlice().decode(address, uint128, uint32, string, string);
+        serviceAddress = msg.sender;
+    }
+
+    function cancel() public onlyOwner {
+        ISubscriptionServiceContract(serviceAddress).selfdelete();
+        selfdestruct(svcparams.to);
+    }
+}
